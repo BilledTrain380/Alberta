b3105554f6d4a51ef3a0e31c057386967cb813ad
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-03 19:49:01 +0100

    Add KDoc to export manager class

--- src/main/kotlin/ch/schulealtendorf/alberta/jasper/ExportManager.kt
+++ src/main/kotlin/ch/schulealtendorf/alberta/jasper/ExportManager.kt
@@ -8,11 +8,21 @@ import java.io.ByteArrayInputStream
 import java.io.InputStream
 
 /**
+ * Abstracts the jasper report api.
+ * 
  * @author nmaerchy
- * @version 0.0.1
+ * @version 1.0.0
  */
 class ExportManager {
-    
+
+    /**
+     * Exports the given {@code report} with jasper reports.
+     * 
+     * @param report data to use in the report
+     * 
+     * @return the generated report
+     * @throws IllegalArgumentException if the source of the report is not found
+     */
     fun export(report: StreamReport): InputStream {
         
         val template = ExportManager::class.java.classLoader.getResourceAsStream(report.source) ?: throw IllegalArgumentException("Source not found on the classpath: source=${report.source}")
