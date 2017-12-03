668fdb239c8244c3009d338ec2786e0d62a798e0
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-03 19:46:12 +0100

    Implement e2e tests for event sheet api

--- src/test/kotlin/ch/schulealtendorf/alberta/e2eTest.kt
+++ src/test/kotlin/ch/schulealtendorf/alberta/e2eTest.kt
@@ -4,44 +4,79 @@ import ch.schulealtendorf.pra.pojo.Competitor
 import ch.schulealtendorf.pra.pojo.EventSheet
 import org.junit.Test
 import java.io.File
+import java.io.InputStream
 
 /**
  * @author nmaerchy
  * @version 0.0.1
  */
-
 class APITest {
 
     @Test
-    fun eventSheetApi() {
-        
+    fun eventSheetApiWithDistance() {
+
         val api = JasperEventSheetAPI()
-        
+
+        val data = eventSheetTestData("Schnelllauf", "60m")
+
+        val report = api.createReport(data)
+
+        "event-sheet-with-distance.pdf".create(report)
+    }
+    
+    @Test
+    fun eventSheetApiMultipleTrialsNoDistance() {
+
+        val api = JasperEventSheetAPI()
+
+        val data = eventSheetTestData("Seilspringen")
+
+        val report = api.createReport(data)
+
+        "event-sheet-multiple-trials-no-distance.pdf".create(report)
+    }
+
+    @Test
+    fun eventSheetApiMultipleTrialsWithDistance() {
+
+        val api = JasperEventSheetAPI()
+
+        val data = eventSheetTestData("Seilspringen", "2.5m")
+
+        val report = api.createReport(data)
+
+        "event-sheet-multiple-trials-with-distance.pdf".create(report)
+    }
+    
+    private fun eventSheetTestData(discipline: String, distance: String? = null): EventSheet {
+
         val competitor1 = Competitor()
         competitor1.startnumber = 1
         competitor1.prename = "Max"
         competitor1.surname = "Muster"
-        competitor1.distance = "60m"
+        competitor1.distance = distance
 
         val competitor2 = Competitor()
         competitor2.startnumber = 2
         competitor2.prename = "Sascha"
         competitor2.surname = "Suster"
-        competitor2.distance = "60m"
-        
+        competitor2.distance = distance
+
         val data = EventSheet()
-        data.discipline = "Schnelllauf"
+        data.discipline = discipline
         data.clazz = "2a"
-        data.isGender = true
+        data.isGender = false
         data.competitors = listOf(
                 competitor1,
                 competitor2
         )
         
-        val report = api.createReport(data)
-        
-        val file = File("e2e/event-sheet.pdf")
+        return data
+    }
+    
+    private fun String.create(stream: InputStream) {
+        val file = File("e2e/$this")
         file.createNewFile()
-        file.writeBytes(report.readBytes())
+        file.writeBytes(stream.readBytes())
     }
 }
