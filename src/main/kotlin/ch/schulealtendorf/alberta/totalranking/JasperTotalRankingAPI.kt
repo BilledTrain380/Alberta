d13157ff23d9476b715e9a631f8283a5a7d0ecdb
Nicolas Märchy <nm@studer-raimann.ch>
2018-01-27 21:31:45 +0100

    Create total ranking class structure

---
+++ src/main/kotlin/ch/schulealtendorf/alberta/totalranking/JasperTotalRankingAPI.kt
@@ -0,0 +1,19 @@
+package ch.schulealtendorf.alberta.totalranking
+
+import ch.schulealtendorf.pra.api.TotalRankingAPI
+import ch.schulealtendorf.pra.pojo.TotalRanking
+import java.io.InputStream
+
+/**
+ * @author nmaerchy
+ * @version 0.0.1
+ */
+class JasperTotalRankingAPI: TotalRankingAPI {
+
+    override fun createReport(p0: TotalRanking?): InputStream {
+        
+         
+        
+        throw UnsupportedOperationException("This method is not implemented yet.") //To change body of created functions use File | Settings | File Templates.
+    }
+}
